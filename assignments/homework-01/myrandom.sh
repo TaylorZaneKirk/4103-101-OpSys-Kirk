5d3171884d7aa62f3c5d43d548cc69029cb560ac
Taylor Kirk <TaylorZaneKirk@gmail.com>
2016-09-08 01:34:23 -0500

    testing

--- assignments/homework-01/myrandom.sh
+++ assignments/homework-01/myrandom.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-output = "there was an error."
-shuf -n 1 /usr/share/dict/words > output
-cat output
+$output = "there was an error."
+shuf -n 1 /usr/share/dict/words > $output
+cat $output
