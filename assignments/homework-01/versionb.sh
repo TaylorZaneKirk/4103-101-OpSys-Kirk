d8fef9d6c54e90a6f43977605a2eb1470e87875a
Taylor Kirk <TaylorZaneKirk@gmail.com>
2016-09-08 10:40:13 -0500

    Script4 Done?

--- assignments/homework-01/versionb.sh
+++ assignments/homework-01/versionb.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
 origFile=$1
-baseFile=$(basename $origFile | cut -d. -f2)
-echo $baseFile
+baseFile=$(basename $origFile | cut -d. -f1)
+baseExtension=$(basename $origFile | cut -d. -f2)
+today=$(date +'%Y_%m_%d')
+
+newFile=$baseFile'_'$today'.'$baseExtension
+cp $1 ./$newFile
+echo 'Done'
